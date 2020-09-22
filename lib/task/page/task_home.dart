14f0ee3a51e4fc9cdce82cfc46a60f4ffe0c0f2c
sangxiang <1005015165@qq.com>
2020-09-22 09:25:54 +0800

    完成首页轮播组件开发

--- lib/task/page/task_home.dart
+++ lib/task/page/task_home.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-
 class TaskHomePage extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
@@ -14,17 +13,48 @@ class TaskHomeStatePage extends State<TaskHomePage> with AutomaticKeepAliveClien
 
   @override
   bool get wantKeepAlive => true;
+  List<Image> imgs = [
+    //建立了一个图片数组
+    Image.network(
+      "https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      fit: BoxFit.cover,
+    ),
+    Image.network(
+      "https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+      fit: BoxFit.cover,
+    ),
+    Image.network(
+      "https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      fit: BoxFit.cover,
+    ),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
-    return new Swiper(
-      itemBuilder: (BuildContext context,int index){
-        return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
-      },
-      itemCount: 3,
-      pagination: new SwiperPagination(),
-      control: new SwiperControl(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("图片轮播"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              child: Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  //条目构建函数传入了index,根据index索引到特定图片
+                  return imgs[index];
+                },
+                itemCount: imgs.length,
+                autoplay: true,
+                pagination: new SwiperPagination(), //这些都是控件默认写好的,直接用
+                control: new SwiperControl( iconNext: null,iconPrevious: null),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
 }
